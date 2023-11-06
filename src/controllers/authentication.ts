877a1f0802bfbac6dd964d13b3e124fb07d929fc
anishmitagar <Anish.S.Mitagar@gmail.com>
2023-11-06 10:56:43 -0500

    updated domain auth

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -59,7 +59,7 @@ export const login = async (req: express.Request, res: express.Response) => {
 
         await user.save();
 
-        res.cookie('ANISH-AUTH', user.authentication.sessionToken, { domain: 'localhost', path: '/' });
+        res.cookie('ANISH-AUTH', user.authentication.sessionToken, { domain: 'https://planpal-backend-xig7.onrender.com', path: '/' });
 
         return res.status(200).json(user).end();
     } catch (error) {
